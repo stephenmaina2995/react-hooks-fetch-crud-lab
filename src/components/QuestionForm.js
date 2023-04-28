0f746ced5f0642cf4a899e4200b94a51a5b1815d
stephenmaina2995 <stephenmaina2995@gmail.com>
2023-04-28 14:59:44 +0300

    Completed assignment

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,25 +1,41 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
 function QuestionForm(props) {
   const [formData, setFormData] = useState({
-    prompt: "",
-    answer1: "",
-    answer2: "",
-    answer3: "",
-    answer4: "",
+    prompt: '',
+    answer1: '',
+    answer2: '',
+    answer3: '',
+    answer4: '',
     correctIndex: 0,
-  });
+  })
 
   function handleChange(event) {
     setFormData({
       ...formData,
       [event.target.name]: event.target.value,
-    });
+    })
   }
 
   function handleSubmit(event) {
-    event.preventDefault();
-    console.log(formData);
+    event.preventDefault()
+    //console.log(formData);
+    fetch('http://localhost:4000/questions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        prompt: formData.prompt,
+        answers: [
+          formData.answer1,
+          formData.answer2,
+          formData.answer3,
+          formData.answer4,
+        ],
+        correctIndex: parseInt(formData.correctIndex),
+      }),
+    })
   }
 
   return (
@@ -29,8 +45,8 @@ function QuestionForm(props) {
         <label>
           Prompt:
           <input
-            type="text"
-            name="prompt"
+            type='text'
+            name='prompt'
             value={formData.prompt}
             onChange={handleChange}
           />
@@ -38,8 +54,8 @@ function QuestionForm(props) {
         <label>
           Answer 1:
           <input
-            type="text"
-            name="answer1"
+            type='text'
+            name='answer1'
             value={formData.answer1}
             onChange={handleChange}
           />
@@ -47,8 +63,8 @@ function QuestionForm(props) {
         <label>
           Answer 2:
           <input
-            type="text"
-            name="answer2"
+            type='text'
+            name='answer2'
             value={formData.answer2}
             onChange={handleChange}
           />
@@ -56,8 +72,8 @@ function QuestionForm(props) {
         <label>
           Answer 3:
           <input
-            type="text"
-            name="answer3"
+            type='text'
+            name='answer3'
             value={formData.answer3}
             onChange={handleChange}
           />
@@ -65,8 +81,8 @@ function QuestionForm(props) {
         <label>
           Answer 4:
           <input
-            type="text"
-            name="answer4"
+            type='text'
+            name='answer4'
             value={formData.answer4}
             onChange={handleChange}
           />
@@ -74,20 +90,20 @@ function QuestionForm(props) {
         <label>
           Correct Answer:
           <select
-            name="correctIndex"
+            name='correctIndex'
             value={formData.correctIndex}
             onChange={handleChange}
           >
-            <option value="0">{formData.answer1}</option>
-            <option value="1">{formData.answer2}</option>
-            <option value="2">{formData.answer3}</option>
-            <option value="3">{formData.answer4}</option>
+            <option value='0'>{formData.answer1}</option>
+            <option value='1'>{formData.answer2}</option>
+            <option value='2'>{formData.answer3}</option>
+            <option value='3'>{formData.answer4}</option>
           </select>
         </label>
-        <button type="submit">Add Question</button>
+        <button type='submit'>Add Question</button>
       </form>
     </section>
-  );
+  )
 }
 
-export default QuestionForm;
+export default QuestionForm
--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React from 'react'
 
-function QuestionItem({ question }) {
-  const { id, prompt, answers, correctIndex } = question;
+function QuestionItem({ question, onDeleteClick, onAnswerChange }) {
+  const { id, prompt, answers, correctIndex } = question
 
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
       {answer}
     </option>
-  ));
+  ))
+  function handleDelete() {
+    onDeleteClick(id)
+  }
+  function handleAnswerChange(event) {
+    onAnswerChange(id, parseInt(event.target.value))
+  }
 
   return (
     <li>
@@ -15,11 +21,13 @@ function QuestionItem({ question }) {
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={handleAnswerChange}>
+          {options}
+        </select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
-  );
+  )
 }
 
-export default QuestionItem;
+export default QuestionItem
